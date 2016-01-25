6b83208707a15290cc1950503125389ea36bdf70
keighty <k80.leonard@gmail.com>
2016-01-25 07:38:50 -0800

    Remodel string concatenation to shut up the linter.

--- config/seeds.js
+++ config/seeds.js
@@ -2,28 +2,28 @@ var connection = require('./schema')
 
 connection.query('USE virtual_playbill', function (err) {
   if (err) throw err;
-  connection.query('INSERT into user ('
-    + 'f_name, l_name'
-    + ') '
-    + 'VALUES ('
-    + '"Foo",'
-    + '"Bar"'
-    + ')', function (err) {
+  connection.query('INSERT into user (' +
+    'f_name, l_name' +
+    ') ' +
+    'VALUES (' +
+    '"Foo",' +
+    '"Bar"' +
+    ')', function (err) {
         if (err) throw err
     });
-  connection.query('INSERT into performance VALUES ('
-    + 'null,'
-    + '"Baba Yaga",'
-    + '"test author",'
-    + '"test director",'
-    + '"test company",'
-    + '"Fertile Ground Festival Venue",'
-    + '"test music",'
-    + '"test choreo",'
-    + '"test synopsis -- morning is wiser than evening",'
-    + '"test category",'
-    + '"test image"'
-    + ')', function (err) {
+  connection.query('INSERT into performance VALUES (' +
+    'null,' +
+    '"Baba Yaga",' +
+    '"test author",' +
+    '"test director",' +
+    '"test company",' +
+    '"Fertile Ground Festival Venue",' +
+    '"test music",' +
+    '"test choreo",' +
+    '"test synopsis -- morning is wiser than evening",' +
+    '"test category",' +
+    '"test image"' +
+    ')', function (err) {
         if (err) throw err
         else connection.end()
     });
