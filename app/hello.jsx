52bc1d6c8505ad1043bdfea86a36ac2c12e7c110
keighty <k80.leonard@gmail.com>
2016-02-01 08:01:58 -0800

    Add views and template markdown for placeholder react components.

---
+++ app/hello.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+/*
+
+FilterablePerformanceGrid
+  SearchBar
+  PerformanceFrame
+    PerformanceTitle
+    PerformanceDate
+    PerformanceImage
+*/
+
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('hello'))
+
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+// ReactDOM.render(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('hello')
+// );
