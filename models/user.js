3c5b9c30bcee3e20ad8ae90a3f429e175740b329
keighty <k80.leonard@gmail.com>
2016-07-19 08:31:10 -0700

    Adjust user model to make use of new db function: performQuery.

--- models/user.js
+++ models/user.js
@@ -1,29 +1,19 @@
 var Database = require('../config/db')
-var db = new Database()
-
-function performQuery (query, cb) {
-  db.connect(function (err) {
-    if (err) cb(err)
-    else db.query(query, function (err, data) {
-      cb(err, data)
-      db.close()
-    })
-  })
-}
+var database = new Database()
 var tableName = 'user'
 var query
 
 module.exports = {
-  db: db,
+  db: database,
 
   all: function (cb) {
     query = ['SELECT * from', tableName, ';'].join(' ')
-    performQuery(query, cb)
+    this.db.performQuery(query, cb)
   },
 
   get: function (id, cb) {
     query = ['SELECT * from', tableName, 'WHERE id =', id, ';'].join(' ')
-    performQuery(query, cb)
+    this.db.performQuery(query, cb)
   },
 
   add: function (user, cb) {
@@ -32,11 +22,11 @@ module.exports = {
       user.l_name
     ].join(', ')
     query = ['INSERT into', tableName, 'VALUES (null,', values, ');'].join(' ')
-    performQuery(query, cb)
+    this.db.performQuery(query, cb)
   },
 
   delete: function (id, cb) {
     query = ['DELETE FROM', tableName, 'WHERE ID=', id, ';'].join(' ')
-    performQuery(query, cb)
+    this.db.performQuery(query, cb)
   }
 }
--- test/server/models/user-model_test.js
+++ test/server/models/user-model_test.js
@@ -1,246 +1,146 @@
-// var expect = require('chai').expect
-// var sinon = require('sinon')
-// var user = require('../../../models/user')
-// var Database = require('../../../config/db')
+var expect = require('chai').expect
+var sinon = require('sinon')
+var user = require('../../../models/user')
+var Database = require('../../../config/db')
 
-// describe('user model', function () {
-//   var mockConnect, sandbox
+describe('user model', function () {
+  var sandbox, mockDb
 
-//   var testData = [
-//     {id: 1, foo: 'bar'},
-//     {id: 2, foo: 'baz'}
-//   ]
+  var testData = [
+    {id: 1, foo: 'bar'},
+    {id: 2, foo: 'baz'}
+  ]
 
-//   beforeEach(function () {
-//     sandbox = sinon.sandbox.create()
-//     mockConnect = user.db.connect = sandbox.spy()
-//     mockQuery = user.db.query = sandbox.spy()
-//   })
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create()
+    mockDb = {
+      performQuery: sandbox.spy()
+    }
+    user.db = mockDb
+  })
 
-//   afterEach(function () {
-//     sandbox.restore()
-//   })
+  afterEach(function () {
+    sandbox.restore()
+  })
 
-//   it('should pass this canary test', function () {
-//     expect(true).to.be.true
-//   })
+  it('should pass this canary test', function () {
+    expect(true).to.be.true
+  })
 
-//   describe('#all', function () {
-//     it('should call connect', function () {
-//       user.all()
+  describe('#all', function () {
+    it('should call performQuery', function () {
+      var cb = function () {}
+      user.all(cb)
 
-//       expect(mockConnect.called).to.be.true
-//     })
+      expect(mockDb.performQuery.called).to.be.true
+    })
 
-//     it('should handle a connection error', function (done) {
-//       var cb = function (err) {
-//         expect(err.message).to.be.eql('Could not connect')
-//         done()
-//       }
+    it('should return all users', function (done) {
+      var cb = function (err, data) {
+        expect(data).to.be.eql(testData)
+        done()
+      }
+      user.all(cb)
 
-//       user.all(cb)
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback(new Error('Could not connect'))
-//     })
+      var registeredQueryCallback = mockDb.performQuery.firstCall.args[1]
+      registeredQueryCallback(null, testData)
+    })
 
-//     it('should call query', function () {
-//       var cb = function () {}
-//       user.all()
+    it('should handle query errors', function (done) {
+      var cb = function (err, data) {
+        expect(err.message).to.be.eql('Bad query')
+        done()
+      }
+      user.all(cb)
 
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
+      var registeredQueryCallback = mockDb.performQuery.firstCall.args[1]
+      registeredQueryCallback(new Error('Bad query'))
+    })
+  })
 
-//       expect(mockQuery.called).to.be.true
-//     })
+  describe('#get', function () {
+    it('should call performQuery', function () {
+      user.get()
 
-//     it('should return all users', function (done) {
-//       var cb = function (err, data) {
-//         expect(data).to.be.eql(testData)
-//         done()
-//       }
-//       user.all(cb)
+      expect(mockDb.performQuery.called).to.be.true
+    })
 
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
+    it('should return a specific user', function (done) {
+      var cb = function (err, data) {
+        expect(data).to.be.eql([testData[0]])
+        done()
+      }
+      user.get(1, cb)
 
-//       var registeredQueryCallback = mockQuery.firstCall.args[1]
-//       registeredQueryCallback(null, testData)
-//     })
+      var registeredQueryCallback = mockDb.performQuery.firstCall.args[1]
+      registeredQueryCallback(null, [testData[0]])
+    })
 
-//     it('should handle query errors', function (done) {
-//       var cb = function (err, data) {
-//         expect(err.message).to.be.eql('Bad query')
-//         done()
-//       }
-//       user.all(cb)
+    it('should handle query errors', function (done) {
+      var cb = function (err, data) {
+        expect(err.message).to.be.eql('Bad query')
+        done()
+      }
+      user.get(1, cb)
 
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
+      var registeredQueryCallback = mockDb.performQuery.firstCall.args[1]
+      registeredQueryCallback(new Error('Bad query'))
+    })
+  })
 
-//       var registeredQueryCallback = mockQuery.firstCall.args[1]
-//       registeredQueryCallback(new Error('Bad query'))
-//     })
-//   })
+  describe('#add', function () {
+    var testUser = {
+      f_name: 'foo',
+      l_name: 'bar'
+    }
 
-//   describe('#get', function () {
-//     it('should call connect', function () {
-//       user.get()
+    it('should call performQuery', function () {
+      var cb = function () {}
+      user.add(testUser)
 
-//       expect(mockConnect.called).to.be.true
-//     })
+      expect(mockDb.performQuery.called).to.be.true
+    })
 
-//     it('should handle a connection error', function (done) {
-//       var cb = function (err) {
-//         expect(err.message).to.be.eql('Could not connect')
-//         done()
-//       }
+    it('should return a specific user', function (done) {
+      var cb = function (err, data) {
+        expect(data).to.be.eql('success')
+        done()
+      }
+      user.add(testUser, cb)
 
-//       user.get(1, cb)
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback(new Error('Could not connect'))
-//     })
+      var registeredQueryCallback = mockDb.performQuery.firstCall.args[1]
+      registeredQueryCallback(null, 'success')
+    })
 
-//     it('should call query', function () {
-//       var cb = function () {}
-//       user.get(1)
+    it('should handle query errors', function (done) {
+      var cb = function (err, data) {
+        expect(err.message).to.be.eql('Bad query')
+        done()
+      }
+      user.add(testUser, cb)
 
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
+      var registeredQueryCallback = mockDb.performQuery.firstCall.args[1]
+      registeredQueryCallback(new Error('Bad query'))
+    })
+  })
 
-//       expect(mockQuery.called).to.be.true
-//     })
+  describe('#delete', function () {
+    it('should call performQuery', function () {
+      var cb = function () {}
+      user.delete(1, cb)
 
-//     it('should return a specific user', function (done) {
-//       var cb = function (err, data) {
-//         expect(data).to.be.eql([testData[0]])
-//         done()
-//       }
-//       user.get(1, cb)
+      expect(mockDb.performQuery.called).to.be.true
+    })
 
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
+    it('should handle query errors', function (done) {
+      var cb = function (err, data) {
+        expect(err.message).to.be.eql('Bad query')
+        done()
+      }
+      user.delete(1, cb)
 
-//       var registeredQueryCallback = mockQuery.firstCall.args[1]
-//       registeredQueryCallback(null, [testData[0]])
-//     })
-
-//     it('should handle query errors', function (done) {
-//       var cb = function (err, data) {
-//         expect(err.message).to.be.eql('Bad query')
-//         done()
-//       }
-//       user.get(1, cb)
-
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
-
-//       var registeredQueryCallback = mockQuery.firstCall.args[1]
-//       registeredQueryCallback(new Error('Bad query'))
-//     })
-//   })
-
-//   describe('#add', function () {
-//     var testUser = {
-//       f_name: 'foo',
-//       l_name: 'bar'
-//     }
-
-//     it('should call connect', function () {
-//       user.add(testUser)
-
-//       expect(mockConnect.called).to.be.true
-//     })
-
-//     it('should handle a connection error', function (done) {
-//       var cb = function (err) {
-//         expect(err.message).to.be.eql('Could not connect')
-//         done()
-//       }
-
-//       user.add(testUser, cb)
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback(new Error('Could not connect'))
-//     })
-
-//     it('should call query', function () {
-//       var cb = function () {}
-//       user.add(testUser)
-
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
-
-//       expect(mockQuery.called).to.be.true
-//     })
-
-//     it('should return a specific user', function (done) {
-//       var cb = function (err, data) {
-//         expect(data).to.be.eql('success')
-//         done()
-//       }
-//       user.add(testUser, cb)
-
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
-
-//       var registeredQueryCallback = mockQuery.firstCall.args[1]
-//       registeredQueryCallback(null, 'success')
-//     })
-
-//     it('should handle query errors', function (done) {
-//       var cb = function (err, data) {
-//         expect(err.message).to.be.eql('Bad query')
-//         done()
-//       }
-//       user.add(testUser, cb)
-
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
-
-//       var registeredQueryCallback = mockQuery.firstCall.args[1]
-//       registeredQueryCallback(new Error('Bad query'))
-//     })
-//   })
-
-//   describe('#delete', function () {
-//     it('should call connect', function () {
-//       user.delete(1)
-
-//       expect(mockConnect.called).to.be.true
-//     })
-
-//     it('should handle a connection error', function (done) {
-//       var cb = function (err) {
-//         expect(err.message).to.be.eql('Could not connect')
-//         done()
-//       }
-
-//       user.delete(1, cb)
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback(new Error('Could not connect'))
-//     })
-
-//     it('should call query', function () {
-//       var cb = function () {}
-//       user.delete(1, cb)
-
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
-
-//       expect(mockQuery.called).to.be.true
-//     })
-
-//     it('should handle query errors', function (done) {
-//       var cb = function (err, data) {
-//         expect(err.message).to.be.eql('Bad query')
-//         done()
-//       }
-//       user.delete(1, cb)
-
-//       var registeredCallback = mockConnect.firstCall.args[0]
-//       registeredCallback()
-
-//       var registeredQueryCallback = mockQuery.firstCall.args[1]
-//       registeredQueryCallback(new Error('Bad query'))
-//     })
-//   })
-// })
+      var registeredQueryCallback = mockDb.performQuery.firstCall.args[1]
+      registeredQueryCallback(new Error('Bad query'))
+    })
+  })
+})
