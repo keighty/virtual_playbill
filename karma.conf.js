00405c14cc6e345ff2463d03e051638f76fe04fe
keighty <k80.leonard@gmail.com>
2016-07-24 10:22:02 -0700

    Get client-side tests running.

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        './test/client/*.js',
-        './public/javascripts/*.js',
+        './test/client/**/*.js',
+        // './public/javascripts/*.js',
         './src/**/*.js'
     ],
 
---
+++ test/client/containers/filterable-performance-grid_test.js
@@ -0,0 +1,5 @@
+describe('filterable performance grid', function () {
+  it('should pass this canary test', function () {
+    expect(true).to.eql(true)
+  })
+})
