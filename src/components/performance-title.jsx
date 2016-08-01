7554810b694513f9c9e359d0e4285b0cc4615e28
keighty <k80.leonard@gmail.com>
2016-08-01 07:47:25 -0700

    Add commands for testing mocha with babel.

--- src/components/performance-title.jsx
+++ src/components/performance-title.jsx
@@ -16,4 +16,3 @@ let title = {
 }
 
 export default PerformanceTitle
-
--- test/client/components/performance-title_test.js
+++ test/client/components/performance-title_test.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import {expect} from 'chai'
 import {mount, shallow} from 'enzyme'
+import PerformanceTitle from '../../../src/components/performance-title'
 
-describe('<PerformanceTitle>', () => {
+describe.only('<PerformanceTitle>', () => {
   it('passes this canary test', () => {
     expect(true).to.be.true
   })
