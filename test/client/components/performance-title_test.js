fdb4cea2c4e235a59a78e5f144825f6d259e0d3c
keighty <k80.leonard@gmail.com>
2016-08-01 08:35:16 -0700

    Add complete test for performanceTitle.

--- test/client/components/performance-title_test.js
+++ test/client/components/performance-title_test.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import {expect} from 'chai'
-import {mount, shallow} from 'enzyme'
+import {shallow} from 'enzyme'
 import PerformanceTitle from '../../../src/components/performance-title'
 
 describe.only('<PerformanceTitle>', () => {
-  it('passes this canary test', () => {
+  const props = {
+    title: 'foo',
+  }
+
+  it('should pass this canary test', () => {
     expect(true).to.be.true
+  })
+
+  it('should render an h4', function () {
+    const wrapper = shallow(<PerformanceTitle {...props}/>)
+    const title = wrapper.find('h4').text()
+
+    expect(title).to.be.eql('foo')
   })
 })
