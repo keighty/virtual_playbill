6e9355ac4c70835e63845561340c31b7a95dbdea
keighty <k80.leonard@gmail.com>
2016-08-02 07:54:23 -0700

    Add first test for search input.

---
+++ src/components/search-bar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+class SearchBar extends React.Component {
+  render() {
+    return <input type="text" placeholder='Search shows, actors, company, etc' />
+  }
+}
+
+export default SearchBar
